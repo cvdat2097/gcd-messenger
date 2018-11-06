db9a2305036f77bb5d66504aaf6deaf5ddfd326a
datcv <datcv@vng.com.vn>
2018-11-06 16:59:53 +0700

    search active users

--- src/components/SidePanel/SearchBar/SearchBar.jsx
+++ src/components/SidePanel/SearchBar/SearchBar.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 import './SearchBar.css';
 
 export default class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            keyword: ''
+        }
+
+        this.handleKeywordChange = this.handleKeywordChange.bind(this);
+    }
+
+    handleKeywordChange(e) {
+        this.setState({
+            keyword: e.target.value
+        });        
+        this.props.onKeywordChange(e.target.value);
+    }
+
     render() {
         return (
             <div id="search">
                 <label htmlFor=""><i className="fa fa-search" aria-hidden="true"></i></label>
-                <input type="text" placeholder="Search contacts..." />
+                <input type="text" placeholder="Search contacts..." 
+                value={this.state.keyword}
+                onChange={this.handleKeywordChange}
+                />
             </div>
         );
     }
--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -7,13 +7,47 @@ import ActiveUsers from './ActiveUsers/ActiveUsers';
 import BottomBar from './BottomBar/BottomBar';
 
 export default class SidePanel extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeUsers: props.activeUsers,
+            keyword: ''
+        }
+
+        this.handleKeywordChange = this.handleKeywordChange.bind(this);
+    }
+
+    handleKeywordChange(keyword) {
+        if (!keyword) {
+            this.setState({
+                activeUsers: this.props.activeUsers,
+                keyword: keyword
+            });
+        } else {
+            this.setState({
+                activeUsers: this.props.activeUsers.filter((user) => {
+                    return user.username.toLowerCase().indexOf(keyword) !== -1;
+                }),
+                keyword: keyword
+            })
+        }
+    }
+    componentWillReceiveProps(newProps) {
+        if (newProps.activeUsers.length !== this.state.activeUsers.length) {
+            this.handleKeywordChange(this.state.keyword);
+        }
+    }
+
     render() {
         return (
             <div id="sidepanel">
                 <Profile username={this.props.username} />
-                <SearchBar />
+                <SearchBar
+                    onKeywordChange={this.handleKeywordChange}
+                />
                 <ActiveUsers
-                    activeUsers={this.props.activeUsers}
+                    activeUsers={this.state.activeUsers}
                 />
                 <BottomBar onChangeUsername={this.props.onChangeUsername} />
             </div>
