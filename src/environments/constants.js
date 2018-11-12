e0b4ad7d5a3354b4a342e87e76e9880887ed761f
datcv <datcv@vng.com.vn>
2018-11-12 14:43:36 +0700

    change server config

--- src/environments/constants.js
+++ src/environments/constants.js
@@ -1,9 +1,9 @@
 export const CONSTANTS = {
     N_MESSAGES: 5,
-    SOCKET_SERVER: "http://localhost:8080/websocket",
-    REST_SERVER: "http://localhost:8080/api",
-    MSG_POINT: "/messages",
-    ROOM_NAME: "chatroom",
+    SOCKET_SERVER: "http://localhost:8080/ws",
+    REST_SERVER: "http://localhost:8080/chat",
+    MSG_POINT: "/chat.sendMessage",
+    ROOM_NAME: "/topic/publicChatRoom",
     NEW_MSG: 'server received a new message!',
     NEW_USR: 'new user has been online!'
 }
